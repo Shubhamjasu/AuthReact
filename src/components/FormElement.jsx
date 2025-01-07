e501b5c06af3c644e11a8c1db58df09da5cf20cd
jai <shubham.jaiswal@40bears.com>
2025-01-07 18:15:40 +0530

    fix: added error imoji

--- src/components/FormElement.jsx
+++ src/components/FormElement.jsx
@@ -3,7 +3,7 @@ export default function FormElement({ id, type, title, err, onChange }) {
         <div className="mb-3">
             <label htmlFor={id} className="form-label float-start" >{title}:</label>
             <input type={type} className="form-control" id={id} placeholder={"Enter " + title} name={type} onChange={onChange} />
-            {err[id] && (<div className="form-text text-danger float-start" id="basic-addon4">{err[id]}</div>)}
+            {err[id] && (<div className="form-text text-danger float-start" id="basic-addon4">{err[id] + "⚠️"}</div>)}
         </div>
     )
 }
